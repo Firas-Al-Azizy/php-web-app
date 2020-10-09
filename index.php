0ebce1828d2b11c4585a3b1070649ffa504d77f0
husseinbasha <37039156+husseinbasha@users.noreply.github.com>
2020-10-09 03:04:45 +0300

    dealt with user interface issue

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
  <?php
-        session_start();
-        require '/header.php';
-        require '/nav.php';
-?>
-        <div class="main-w3layouts wrapper" >
-            <h1>
-        <?php
-        
-        if(isset($_SESSION['ID'])){
-            $username = $_SESSION['USERNAME'];
-            echo "hello $username 
-            <a href=\"includes/logut.inc.php\">logout</a>";
+    session_start();
+    require '/header.php';
+    require '/nav.php';
+    ?>
+ <div class="main-w3layouts wrapper">
+     <h1>
+         <?php
 
-        }else {
-            header('Location: login.php?status=waiting-for-login');
-        }echo 'boo';
-        ?>
-        </h1>
+            if (isset($_SESSION['ID'])) {
+                $username = $_SESSION['USERNAME'];
+                echo "hello $username 
+            <a href=\"includes/logut.inc.php\">logout</a>";
+            } else {
+                header('Location: login.php?status=waiting-for-login');
+            }
+            echo 'boo';
+            ?>
+
         </div>
         
-<?php 
-require 'footer.php';
+
+ <?php
+    require 'footer.php';
 ?>
-      
+    
