bb75d131436c4f1c5b4f1b6684bb8a7802933d46
husseinbasha <37039156+husseinbasha@users.noreply.github.com>
2020-10-09 03:02:07 +0300

    concept for article (buggy css)

---
+++ article.php
@@ -0,0 +1,26 @@
+<?php
+
+require '/header.php';
+require '/nav.php';
+
+?>
+<div class="main-w3-layouts wrapper" style="background:#fff">
+    <div class="article" >
+        <h1>
+            hello world
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium et architecto corporis cumque fuga quo sapiente eos perferendis enim consequuntur, aliquid nemo quasi in facere totam. Dolore quod odit enim.
+        </p>
+        <span>
+                <?php 
+                
+                 echo 'date : 1/10/2010'
+                ?>
+            </span>
+    </div>
+</div>
+
+
+
+
