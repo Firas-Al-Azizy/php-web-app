24b6617cc6abc14d896c05588ba765d54f696ac8
husseinbasha <37039156+husseinbasha@users.noreply.github.com>
2020-10-09 03:03:14 +0300

    fixed footer issue

--- footer.php
+++ footer.php
@@ -1,19 +1,4 @@
-        
-        </div>  
-
     
-<ul class="colorlib-bubbles">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-    </ul>
-    </div>
+
 </body>
 </html>
