e033acdff514027220aef4ea5979a9c28c0b999e
husseinbasha <37039156+husseinbasha@users.noreply.github.com>
2020-10-09 03:00:50 +0300

    added loggo brand name and fixed search bar

--- nav.php
+++ nav.php
@@ -1,28 +1,42 @@
-<?php echo'
+
 <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #121212;">
-		<a class="navbar-brand" href="#">YEMEN-business</a>
-		<button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+		<a class="navbar-brand">
+		<i class="fas fa-brain   mr-1 "></i>
+		FEEDIT
+		</a>
+		<button class="navbar-toggler d-lg-none " type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
 			aria-expanded="false" aria-label="Toggle navigation"></button>
 		<div class="collapse navbar-collapse" id="collapsibleNavId">
 			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-				<li class="nav-item ">
+				<li class="nav-item">
 					<a class="nav-link" href="#">Home </a>
 				</li>
+				
+
 				<li class="nav-item">
 					<a class="nav-link" href="#">Featured </a>	
 				</li>
 				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Categories</a>
+					<a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Topics</a>
 					<div class="dropdown-menu" aria-labelledby="dropdownId">
-						<a class="dropdown-item" href="#">Action 1</a>
-						<a class="dropdown-item" href="#">Action 2</a>
+						<a class="dropdown-item" href="#">Computer Science</a>
+						<a class="dropdown-item" href="#">Big data</a>
+						<a class="dropdown-item" href="#">Research papers</a>
+						<a class="dropdown-item" href="#">Spacecrafts</a>
 					</div>
 				</li>
+				<!--TODO profile for user route-->
+
 			</ul>
-			<form class="form-inline my-2 my-lg-0">
-				<input class="form-control mr-sm-2" type="text" placeholder="Search">
-				<button class="btn  my-2 my-sm-0" style="background-color: #76b852;" type="submit">Search</button>
+			<form>
+				<div class="input-group">
+				<input class="form-control bg-light " type="text" placeholder="Search">
+				<div class="input-group-btn bg-dark">
+				<button class="btn btn-dark  " type="submit"><i class="fa fa-search" aria-hidden="true"></i></i>
+				</button>
+				</div>
+				</div>
 			</form>
 		</div>
 	</nav>
-';
+
